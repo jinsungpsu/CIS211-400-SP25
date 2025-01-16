005a03c23ba6de7e8db5dca0a4b06f2dd24006ca
Jin An <jin.an@dtcc.edu>
2025-01-16 14:49:06 -0500

    Create JavaLists.java

---
+++ week01/Thursday/JavaLists.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class JavaLists {
+    public static void main(String[] args) {
+        //ArrayList<Integer> numList = new ArrayList<>();
+        LinkedList<Integer> numList = new LinkedList<>();
+        numList.add(1);
+        numList.add(2);
+        numList.add(3);
+        System.out.println(numList);
+    }
+}
