bcdb8e9d021596d8049d481cd1e961e692c33aa7
Jin An <jin.an@dtcc.edu>
2025-01-21 14:48:18 -0500

    Create ListTester.java

---
+++ Week02/Tuesday/ListTester.java
@@ -0,0 +1,10 @@
+public class ListTester {
+    public static void main(String[] args) {
+        List<Integer> list = new ActualList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.remove(3);
+    }
+}
