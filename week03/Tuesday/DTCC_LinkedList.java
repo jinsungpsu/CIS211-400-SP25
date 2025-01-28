c2ad8ebae1c4a6c3c4b2daa2e47bb6dd278c52d1
Jin An <jin.an@dtcc.edu>
2025-01-28 14:50:17 -0500

    Create DTCC_LinkedList.java

---
+++ week03/Tuesday/DTCC_LinkedList.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String name = null;
+        String blah = "";
+        // int num = null; // this is an error
+        Integer num = null;
+
+        ArrayList<Pokemon> pokelist = new ArrayList<>();
+        pokelist.add(new Pokemon());
+
+
+        DTCC_LinkedList list = new DTCC_LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+    }
+}
+class Pokemon {
+
+}
