634f9c78be15759babc10b5a0725fa79c6021da3
Jin An <jin.an@dtcc.edu>
2025-02-25 14:37:39 -0500

    Create Main.java

---
+++ week07/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList<Integer> dll = new DoublyLinkedList<>();
+        for (int i = 0; i < 200; i++) {
+            dll.add(i);
+        }
+    }
+}
