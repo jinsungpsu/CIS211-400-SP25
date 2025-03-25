c5ad8ab86654e0b079173ff4197bd7a3d27fb0ed
Jin An <jin.an@dtcc.edu>
2025-03-25 14:46:31 -0400

    Create Pokemon.java

---
+++ week10/Tuesday/Pokemon.java
@@ -0,0 +1,15 @@
+public class Pokemon {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Pokemon(String name) {
+        this.name = name;
+    }
+}
